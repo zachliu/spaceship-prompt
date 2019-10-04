921c354f6497f781642d4469df4aa50523d6833f
zachliu <zach.z.liu@gmail.com>
2019-10-04 11:01:26 -0400

    git branch color change according to status

--- sections/git_branch.zsh
+++ sections/git_branch.zsh
@@ -10,7 +10,7 @@
 SPACESHIP_GIT_BRANCH_SHOW="${SPACESHIP_GIT_BRANCH_SHOW=true}"
 SPACESHIP_GIT_BRANCH_PREFIX="${SPACESHIP_GIT_BRANCH_PREFIX="$SPACESHIP_GIT_SYMBOL"}"
 SPACESHIP_GIT_BRANCH_SUFFIX="${SPACESHIP_GIT_BRANCH_SUFFIX=""}"
-SPACESHIP_GIT_BRANCH_COLOR="${SPACESHIP_GIT_BRANCH_COLOR="magenta"}"
+SPACESHIP_GIT_BRANCH_COLOR="${SPACESHIP_GIT_BRANCH_COLOR="green"}"
 
 # ------------------------------------------------------------------------------
 # Section
@@ -23,6 +23,32 @@ spaceship_git_branch() {
 
   local git_current_branch="$vcs_info_msg_0_"
   [[ -z "$git_current_branch" ]] && return
+
+  spaceship::is_git || return
+
+  INDEX=$(command git status --porcelain -b 2> /dev/null)
+
+  # Check for untracked files
+  if $(echo "$INDEX" | command grep -E '^\?\? ' &> /dev/null); then
+    SPACESHIP_GIT_BRANCH_COLOR="red"
+  fi
+
+  # Check for modified files
+  if $(echo "$INDEX" | command grep '^[ MARC]M ' &> /dev/null); then
+    SPACESHIP_GIT_BRANCH_COLOR="red"
+  fi
+
+  # Check for renamed files
+  if $(echo "$INDEX" | command grep '^R[ MD] ' &> /dev/null); then
+    SPACESHIP_GIT_BRANCH_COLOR="red"
+  fi
+
+  # Check for deleted files
+  if $(echo "$INDEX" | command grep '^[MARCDU ]D ' &> /dev/null); then
+    SPACESHIP_GIT_BRANCH_COLOR="red"
+  elif $(echo "$INDEX" | command grep '^D[ UM] ' &> /dev/null); then
+    SPACESHIP_GIT_BRANCH_COLOR="red"
+  fi
 
   git_current_branch="${git_current_branch#heads/}"
   git_current_branch="${git_current_branch/.../}"
